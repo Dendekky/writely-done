f0d28f9a0536d2a005509efc394a1a3028541b41
DESKTOP-TFNDFB9\adeni <adeniyiib360@gmail.com>
2024-10-14 22:54:19 +0100

    create post endpoint for the writing api

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,4 @@
+// import bodyParser from 'body-parser';
 import express, { Express, Request, Response } from 'express';
 import dotenv from 'dotenv';
 import OpenAI from 'openai';
@@ -9,7 +10,8 @@ const openai = new OpenAI({
 });
 
 const app: Express = express();
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 5000;
+app.use(express.json());
 
 app.get('/', (req: Request, res: Response) => {
   res.send('Express + TypeScript Server here');
@@ -23,6 +25,24 @@ app.get('/api/writer', async (req: Request, res: Response) => {
         role: 'user',
         content:
           'Write an essay on the role of ai in content generation. This should be between 200 and 350 words. Be concise and split this into a few paragraphs.',
+      },
+    ],
+  });
+  res.json(completion);
+});
+
+app.post('/api/writer', async (req: Request, res: Response) => {
+  const prompt = `Rewrite the following text in a more formal tone: 
+  ${req.body.text}`;
+
+  console.log(prompt);
+
+  const completion = await openai.chat.completions.create({
+    model: 'gpt-4o-mini',
+    messages: [
+      {
+        role: 'user',
+        content: prompt,
       },
     ],
   });
